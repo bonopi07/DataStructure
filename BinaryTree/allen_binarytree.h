ef8cb4db4a95fcd140a62a222be611452df30768
Seongmin Jeong <min950820@naver.com>
2017-08-08 16:52:21 -0700

    170808 / completed except delete method

--- BinaryTree/allen_binarytree.h
+++ BinaryTree/allen_binarytree.h
@@ -90,32 +90,133 @@ namespace jsm {
 			}
 		}
 	
-		void deleteNode(T _data) {
+		bool search(T _data) {
 			BTNode<T> *curNode = root;
 
 			if (!curNode)
-				return;
+				return false;
 
 			while (true) {
+				// data have to go left-side
 				if (_data < curNode->getData()) {
 					if (curNode->getLeftNode())
 						curNode = curNode->getLeftNode();
 					else
 						break;
 				}
+				// data have to go right-side
 				else if (_data > curNode->getData()) {
 					if (curNode->getRightNode())
 						curNode = curNode->getRightNode();
 					else
 						break;
 				}
-				else {
+				// if data is finded
+				else
+					return true;
+			}
 
+			return false;
+		}
+
+		void deleteNode(T _data) {
+			BTNode<T> *preNode = root;
+			BTNode<T> *curNode = root;
+
+			if (!curNode)
+				return;
+
+			while (true) {
+				// data have to go left-side
+				if (_data < curNode->getData()) {
+					if (curNode->getLeftNode()) {
+						preNode = curNode;
+						curNode = curNode->getLeftNode();
+					}
+					else
+						break;
+				}
+				// data have to go right-side
+				else if (_data > curNode->getData()) {
+					if (curNode->getRightNode()) {
+						preNode = curNode;
+						curNode = curNode->getRightNode();
+					}
+					else
+						break;
+				}
+				// if data is finded
+				else {
+					size -= 1;
+
+					// 1) child node = 0
+					if (curNode->getLeftNode() == NULL && curNode->getRightNode()) {
+						// 부모의 왼쪽 노드가 존재하고
+						if (preNode->getLeftNode()) {
+							// 부모의 왼쪽 노드가 curNode와 같다면
+							if (curNode->getData() == preNode->getLeftNode()->getData()) {
+								preNode->setLeftNode(NULL);
+								delete curNode;
+								return;
+							}
+						}
+						// 그렇지 않다면
+						preNode->setRightNode(NULL);
+						delete curNode;
+					}
+					// 2) child node = 2
+					else if (curNode->getLeftNode() && curNode->getRightNode()) {
+
+					}
+					// 3) child node = 1
+					else {
+
+					}
 				}
 			}
 
 		}
 	
+		BTNode<T>* getRoot() {
+			return this->root;
+		}
+
+		// pre order: VLR
+		void preTraversal(BTNode<T>* _node) {
+			if (_node == NULL)
+				return;
+
+			std::cout << _node->getData() << " ";
+			preTraversal(_node->getLeftNode());
+			preTraversal(_node->getRightNode());
+		}
+
+		// in order: LVR
+		void inTraversal(BTNode<T>* _node) {
+			if (_node == NULL)
+				return;
+
+			inTraversal(_node->getLeftNode());
+			std::cout << _node->getData() << " ";
+			inTraversal(_node->getRightNode());
+		}
+
+		// post order: LRV
+		void postTraversal(BTNode<T>* _node, bool delFlag = false) {
+			if (_node == NULL)
+				return;
+
+			postTraversal(_node->getLeftNode(), delFlag);
+			postTraversal(_node->getRightNode(), delFlag);
+			if (delFlag)
+				delete _node;
+			else
+				std::cout << _node->getData() << " ";
+		}
+
+		~BinarySearchTree() {
+			postTraversal(getRoot(), true);
+		}
 	};
 }
 
--- BinaryTree/driver.cpp
+++ BinaryTree/driver.cpp
@@ -5,10 +5,38 @@ int main()
 {
 	jsm::BinarySearchTree<int> bst;
 
-	bst.insertNode(7);
-	bst.insertNode(5);
-	bst.insertNode(8);
+	std::cout << "insert: 10, 7, 1, 5, 9, 13, 15, 12, 14, 16" << std::endl;
 	bst.insertNode(10);
+	bst.insertNode(7);
+	bst.insertNode(1);
+	bst.insertNode(5);
+	bst.insertNode(9);
+	bst.insertNode(13);
+	bst.insertNode(15);
+	bst.insertNode(12);
+	bst.insertNode(14);
+	bst.insertNode(16);
 
+	std::cout << std::endl;
+	std::cout << "Pre Traversal: ";
+	bst.preTraversal(bst.getRoot());
+	std::cout << std::endl;
+	std::cout << "Answer       : 10 7 1 5 9 13 12 15 14 16 " << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "In Traversal: ";
+	bst.inTraversal(bst.getRoot());
+	std::cout << std::endl;
+	std::cout << "Answer      : 1 5 7 9 10 12 13 14 15 16 " << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "Post Traversal: ";
+	bst.postTraversal(bst.getRoot());
+	std::cout << std::endl;
+	std::cout << "Answer        : 5 1 9 7 12 14 16 15 13 10 " << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "search(14, 9, 3): " << bst.search(14) << " " << bst.search(9) << " " << bst.search(3) << std::endl;
+	
 	return 0;
 }
